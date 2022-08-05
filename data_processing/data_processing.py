f356de27a29539eea635a0339c968732e8afdabc
Steve <steve.gehly@gmail.com>
2022-08-05 13:23:01 +0200

    SP3 Data Reader Working

--- data_processing/data_processing.py
+++ data_processing/data_processing.py
@@ -1,13 +1,44 @@
 import numpy as np
-from math import *
-import pandas as pd
-import os
+import math
 from datetime import datetime
+import matplotlib.pyplot as plt
+import os
+import sys
+import inspect
+
+filename = inspect.getframeinfo(inspect.currentframe()).filename
+current_dir = os.path.dirname(os.path.abspath(filename))
+
+ind = current_dir.find('metis')
+metis_dir = current_dir[0:ind+5]
+sys.path.append(metis_dir)
+
+
+import utilities.coordinate_systems as coord
+import utilities.eop_functions as eop
+import utilities.time_systems as timesys
+import utilities.tle_functions as tle
+
 
 
 def read_sp3_file(sp3_fname):
+    '''
+    This function reads an SP3 data file to retrieve precise orbit data for
+    GNSS (or other) satellites. Data is reformatted into a dictionary for
+    output.
     
-    
+    Parameters
+    ------
+    sp3_fname : string
+        path and filename of SP3 data file to read
+        
+    Returns
+    ------
+    output_dict : dict
+        dictionary indexed by object id (from SP3 file, not NORAD ID) with
+        GPS times, ECEF positions, and Clock Offsets
+
+    '''
     
     # Open data file for read
     sp3_file = open(sp3_fname, 'r')
@@ -15,8 +46,7 @@ def read_sp3_file(sp3_fname):
     # Read lines
     all_lines = sp3_file.readlines()
     sp3_file.close()
-    
-#    dt_list = []
+
     output_dict = {}
     for ii in range(len(all_lines)):
         
@@ -31,7 +61,7 @@ def read_sp3_file(sp3_fname):
             minute = int(line[17:19])
             second = float(line[20:31])
             
-            micro, sec = modf(second)
+            micro, sec = math.modf(second)
             micro = int(micro * 1000000)
             sec = int(sec)
         
@@ -54,36 +84,125 @@ def read_sp3_file(sp3_fname):
             x = float(line[5:18])
             y = float(line[19:32])
             z = float(line[33:46])
-            clock = float(line[47:60])
+            clock = float(line[47:60])/1e6   # convert to seconds
             
             r_ecef = np.reshape([x, y, z], (3,1))
             
             if obj_id not in output_dict:
                 output_dict[obj_id] = {}
-                output_dict[obj_id]['datetime'] = []
+                output_dict[obj_id]['gps_time'] = []
                 output_dict[obj_id]['r_ecef'] = []
-                output_dict[obj_id]['clock_offset'] = []
+                output_dict[obj_id]['clock_offset_sec'] = []
                 
-            output_dict[obj_id]['datetime'].append(dt)
+            output_dict[obj_id]['gps_time'].append(dt)
             output_dict[obj_id]['r_ecef'].append(r_ecef)
-            output_dict[obj_id]['clock_offset'].append(clock)
+            output_dict[obj_id]['clock_offset_sec'].append(clock)
 
     
     return output_dict
 
 
-
-if __name__ == '__main__':
+def unit_test_sp3_reader():
     
-    
-    fdir = 'D:\documents\\teaching\\unsw_ssa_undergrad\lab\\telescope\\truth_data'
-    fname = 'gbm20692.sp3'
+    fdir = 'unit_test'
+    fname = 'qzf22081.sp3'
     sp3_fname = os.path.join(fdir, fname)
     
     sp3_dict = read_sp3_file(sp3_fname)
     
+    # Retrieve latest EOP data from celestrak.com
+    eop_alldata = eop.get_celestrak_eop_alldata()
+        
+    # Retrieve polar motion data from file
+    XYs_df = eop.get_XYs2006_alldata()
     
-    print(sp3_dict['PJ07'])
+    qzs1_norad = 37158
+    qzs2_norad = 42738
+    qzs3_norad = 42917
+    qzs4_norad = 42965
+    
+    qzs3_sp3_id = 'PJ07'
+    
+    norad_id = qzs3_norad
+    sp3_id = qzs3_sp3_id
+    
+    # Convert SP3 data to UTC and ECI
+    gps_list = sp3_dict[sp3_id]['gps_time']
+    ecef_list = sp3_dict[sp3_id]['r_ecef']
+    clock_list = sp3_dict[sp3_id]['clock_offset_sec']
+    UTC_list = []
+    ECI_list = []
+    for ii in range(len(gps_list)):
+        
+        gps_time = gps_list[ii]
+        sp3_ecef = ecef_list[ii]
+        EOP_data = eop.get_eop_data(eop_alldata, gps_time)
+        
+        # Convert to UTC
+        UTC = timesys.gpsdt2utcdt(gps_time, EOP_data['TAI_UTC'])
+        UTC_list.append(UTC) # + timedelta(seconds=clock)
+        
+        # Convert to GCRF
+        sp3_eci, dum = coord.itrf2gcrf(sp3_ecef, np.zeros((3,1)), UTC, EOP_data, XYs_df)
+        ECI_list.append(sp3_eci)
+        
+    # Retrieve and propagate TLE data to desired times
+    tle_state = tle.propagate_TLE([norad_id], UTC_list)
+    
+    # Compute errors at each time
+    thrs = []
+    ric_err = np.zeros((3, len(UTC_list)))
+    for ii in range(len(UTC_list)):
+        
+        UTC = UTC_list[ii]
+        thrs.append((UTC - UTC_list[0]).total_seconds()/3600.)
+        
+        sp3_r_eci = ECI_list[ii].reshape(3,1)
+        tle_r_eci = tle_state[norad_id]['r_GCRF'][ii].reshape(3,1)
+        tle_v_eci = tle_state[norad_id]['v_GCRF'][ii].reshape(3,1)
+        
+        # Compute RIC errors with TLE data acting as chief satellite
+        rho_eci = sp3_r_eci - tle_r_eci    
+        rho_ric = coord.eci2ric(tle_r_eci, tle_v_eci, rho_eci)
+        
+        # Change sign to set SP3 data as chief (truth)
+        rho_ric = -rho_ric      
+        
+        # Store output
+        ric_err[:,ii] = rho_ric.flatten()
+        
+    # Generate plots
+    plt.figure()  
+    plt.subplot(3,1,1)
+    plt.plot(thrs, ric_err[0,:], 'k.')
+    plt.ylabel('Radial Error [km]')
+    plt.title('RIC Error TLE vs SP3')
+    plt.subplot(3,1,2)
+    plt.plot(thrs, ric_err[1,:], 'k.')
+    plt.ylabel('In-Track Error [km]')
+    plt.subplot(3,1,3)
+    plt.plot(thrs, ric_err[2,:], 'k.')
+    plt.ylabel('Cross-Track Error [km]')
+    plt.xlabel('Time Since ' + UTC_list[0].strftime('%Y-%m-%d %H:%M:%S') + ' [hours]')
+    
+    plt.show()
+    
+    
+        
+    
+    
+    
+    
+    
+    
+    return
+
+
+
+if __name__ == '__main__':
+    
+    unit_test_sp3_reader()
+    
     
     
     
