8197a42d6bafae2f873e6bd28a63aa64f200bfcf
Steve <steve.gehly@gmail.com>
2022-08-12 08:23:47 +0200

    IOD Functions

---
+++ iod/iod_functions.py
@@ -0,0 +1,19 @@
+import numpy as np
+from math import exp
+from scipy.integrate import odeint, ode
+import sys
+import os
+import inspect
+from datetime import datetime, timedelta
+
+filename = inspect.getframeinfo(inspect.currentframe()).filename
+current_dir = os.path.dirname(os.path.abspath(filename))
+
+ind = current_dir.find('metis')
+metis_dir = current_dir[0:ind+5]
+sys.path.append(metis_dir)
+
+import dynamics.numerical_integration as numint
+import utilities.astrodynamics as astro
+
+
