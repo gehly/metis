fb4dd44fa409465d00e2e9828ee4215d4cb3adaa
Steve <steve.gehly@gmail.com>
2022-08-13 15:09:34 +0200

    Robust Lambert

--- iod/iod_functions.py
+++ iod/iod_functions.py
@@ -179,7 +179,6 @@ def fast_lambert(r0_vect, rf_vect, tof, m, GM, transfer_type, branch):
     rf_norm = np.linalg.norm(rf_vect)
     dtheta = math.acos(max(-1, min(1, np.dot(r0_vect.T, rf_vect)/rf_norm)))
     
-    
     # Check for Type I (short) or II (long) transfer and adjust
     type_factor = 1.
     if transfer_type == 2:
@@ -198,7 +197,6 @@ def fast_lambert(r0_vect, rf_vect, tof, m, GM, transfer_type, branch):
 
     r_cross = np.linalg.norm(r_cross_vect)
     r_cross_hat = r_cross_vect/r_cross                          # unit vector
-
 
     # Setup initial values
     if m == 0:
@@ -221,8 +219,7 @@ def fast_lambert(r0_vect, rf_vect, tof, m, GM, transfer_type, branch):
             inn2 = 0.5234           # second initial guess
             
         x1 = math.tan(inn1 + math.pi/2.)    # transformed first initial guess
-        x2 = math.tan(inn2 + math.pi/2.)    # transformed first second guess
-            
+        x2 = math.tan(inn2 + math.pi/2.)    # transformed first second guess     
         
     # Initial guess
     xx = np.array([inn1, inn2])
@@ -374,73 +371,375 @@ def fast_lambert(r0_vect, rf_vect, tof, m, GM, transfer_type, branch):
     return v0_vect, vf_vect, extremal_distances, exit_flag
 
 
-#def robust_lambert(r0_vect, rf_vect, tof, m, GM, transfer_type, branch):
-#    '''
-#    This function implements a robust method to solve Lambert's Problem based 
-#    on work by Lancaster and Blanchard, and Gooding. 
-#    
-#    Parameters
-#    ------
-#    r0_vect : 3x1 numpy array
-#        position vector at t0 [km]
-#    rf_vect : 3x1 numpy array
-#        position vector at tf [km]
-#    tof : float
-#        time of flight [sec]
-#    m : int
-#        number of complete orbit revolutions
-#    GM : float
-#        graviational parameter of central body [km^3/s^2]
-#        
-#    Returns
-#    ------
-#    v0_vect : 3x1 numpy array
-#        velocity vector at t0 [km/s]
-#    vf_vect : 3x1 numpy array
-#        velocity vector at tf [km/s]
-#    extremal_distances : list
-#        min and max distance from central body during orbit [km]
-#    exit_flag : int
-#        +1 : success
-#        -1 : fail
-#    '''
-#    
-#    
-#    
-#    # Initialize and normalize values
-#    r0_vect = np.reshape(r0_vect, (3,1))
-#    rf_vect = np.reshape(rf_vect, (3,1))
-#    tol     = 1e-12                            % optimum for numerical noise v.s. actual precision
-#    r0      = np.linalg.norm(r0_vect)              % magnitude of r1vec
-#    rf      = np.linalg.norm(rf_vect)              % magnitude of r2vec
-#    r0_hat  = r0_vect/r0                        % unit vector of r1vec
-#    rf_hat  = rf_vect/rf                         % unit vector of r2vec
-#    crsprod = np.cross(r0_vect, rf_vect);           % cross product of r1vec and r2vec
-#    mcrsprd = sqrt(crsprod*crsprod.');          % magnitude of that cross product
-#    th1unit = cross(crsprod/mcrsprd, r1unit);   % unit vectors in the tangential-directions
-#    th2unit = cross(crsprod/mcrsprd, r2unit);
-#    % make 100.4% sure it's in (-1 <= x <= +1)
-#    dth = acos( max(-1, min(1, (r1vec*r2vec.')/r1/r2)) ); % turn angle
-#
-#    % if the long way was selected, the turn-angle must be negative
-#    % to take care of the direction of final velocity
-#    longway = sign(tf); tf = abs(tf);
-#    if (longway < 0), dth = dth-2*pi; end
-#
+def robust_lambert(r0_vect, rf_vect, tof, m, GM, transfer_type, branch):
+    '''
+    This function implements a robust method to solve Lambert's Problem based 
+    on work by Lancaster and Blanchard, and Gooding. 
+    
+    Parameters
+    ------
+    r0_vect : 3x1 numpy array
+        position vector at t0 [km]
+    rf_vect : 3x1 numpy array
+        position vector at tf [km]
+    tof : float
+        time of flight [sec]
+    m : int
+        number of complete orbit revolutions
+    GM : float
+        graviational parameter of central body [km^3/s^2]
+        
+    Returns
+    ------
+    v0_vect : 3x1 numpy array
+        velocity vector at t0 [km/s]
+    vf_vect : 3x1 numpy array
+        velocity vector at tf [km/s]
+    extremal_distances : list
+        min and max distance from central body during orbit [km]
+    exit_flag : int
+        +1 : success
+        -1 : fail
+    '''
+    
+    
+    
+    # Initialize and normalize values
+    r0_vect = np.reshape(r0_vect, (3,1))
+    rf_vect = np.reshape(rf_vect, (3,1))
+    tol = 1e-12                            # optimum for numerical noise v.s. actual precision
+    r0 = np.linalg.norm(r0_vect)              
+    rf = np.linalg.norm(rf_vect)              
+    r0_hat = r0_vect/r0                        
+    rf_hat = rf_vect/rf                         
+    cross_r0rf = np.reshape(np.cross(r0_vect.flatten(), rf_vect.flatten()), (3,1))
+    cross_mag = np.linalg.norm(cross_r0rf)
+    cross_r0rf_hat = cross_r0rf/cross_mag
+    
+    # Compute unit vectors in tangential direction
+    t0_hat = np.reshape(np.cross(cross_r0rf_hat.flatten(), r0_hat.flatten()), (3,1))
+    tf_hat = np.reshape(np.cross(cross_r0rf_hat.flatten(), rf_hat.flatten()), (3,1))
+    
+    # Compute turn angle
+    dtheta = math.acos(max(-1, min(1, np.dot(r0_hat.T, rf_hat))))
+    
+    # Check for Type I (short) or II (long) transfer and adjust
+    if transfer_type == 2:
+        dtheta = dtheta - 2.*math.pi
+    
+    
+    
+
 #    % left-branch
 #    leftbranch = sign(m); m = abs(m);
-#
-#    % define constants
-#    c  = sqrt(r1^2 + r2^2 - 2*r1*r2*cos(dth));
-#    s  = (r1 + r2 + c) / 2;
-#    T  = sqrt(8*muC/s^3) * tf;
-#    q  = sqrt(r1*r2)/s * cos(dth/2);
-#    
-#    
-#    
-#    
-#    
-#    return v0_vect, vf_vect, extremal_distances, exit_flag
+
+    
+    # Define constants
+    c = np.sqrt(r0**2. + rf**2. - 2.*r0*rf*math.cos(dtheta))
+    s = (r0 + rf + c)/2.
+    T = np.sqrt(8.*GM/s**3.) * tof
+    q = np.sqrt(r0*rf)/s * math.cos(dtheta/2.)
+    
+    # General formulae for initial values (Gooding)
+    T0, dT0, ddT0, dddT0 = LancasterBlanchard(0., q, m)
+    Td = T0 - T
+    phr = math.fmod(2.*math.atan2(1. - q**2., 2.*q), 2.*math.pi)
+    
+    # Initial output
+    v0_vect = np.reshape([np.nan]*3, (3,1))
+    vf_vect = np.reshape([np.nan]*3, (3,1))
+    extremal_distances = [np.nan]*2
+    
+    # Single revolution case
+    if m == 0:
+        x01 = T0*Td/4./T
+        if Td > 0.:
+            x0 = x01
+        else:
+            x01 = Td/(4. - Td)
+            x02 = -np.sqrt(-Td/(T + T0/2.))
+            W = x01 + 1.7*np.sqrt(2. - phr/math.pi)
+            if W >= 0.:
+                x03 = x01
+            else:
+                x03 = x01 + (-W)**(1./16.)*(x02 - x01)
+            
+            Lambda = 1. + x03*(1. + x01)/2. - 0.03*x03**2.*np.sqrt(1. + x01)
+            x0 = Lambda*x03
+            
+        # This estimate might not give a solution
+        if x0 < -1.:
+            exit_flag = -1
+            return v0_vect, vf_vect, extremal_distances, exit_flag
+        
+    # Multi-revolution case
+    else:
+        
+        # Determine minimum Tp(x)
+        xMpi = 4./(3.*math.pi*(2.*m + 1.))
+        if phr < math.pi:
+            xM0 = xMpi*(phr/pi)**(1./8.)
+        elif phr > math.pi:
+            xM0 = xMpi*(2. - (2. - phr/math.pi)**(1./8.))
+        else:
+            xM0 = 0.
+            
+        # Use Halley's method
+        xM = xM0
+        Tp = np.inf
+        iters = 0
+        while abs(Tp) < tol:
+            
+            # Increment counter
+            iters += 1
+            
+            # Compute first three derivatives
+            dum, Tp, Tpp, Tppp = LancasterBlanchard(xM, q, m)
+            
+            # New value of xM
+            xMp = float(xM)
+            xM = xM - 2.*Tp*Tpp / (2.*Tpp**2. - Tp*Tppp)
+            
+            # Escape clause
+            if math.fmod(iters, 7):
+                xM = (xMp + xM)/2.
+            
+            # The method might fail
+            if iters > 25:
+                exit_flag = -2
+                return v0_vect, vf_vect, extremal_distances, exit_flag
+            
+        # xM should be elliptic (-1 < x < 1)
+        # This should be impossible to go wrong
+        if xM < -1. or xM > 1.:
+            exit_flag = -1
+            return v0_vect, vf_vect, extremal_distances, exit_flag
+        
+        # Corresponding time
+        TM, dum1, dum2, dum3 = LancasterBlanchard(xM, q, m)
+        
+        # T should lie above the minimum T
+        if TM > T:
+            exit_flag = -1
+            return v0_vect, vf_vect, extremal_distances, exit_flag
+        
+        
+        # Find two initial values for second solution (again with lambda-type patch)
+        
+        # Initial values
+        TmTM = T - TM
+        T0mTM = T0 - TM
+        dum1, Tp, Tpp, dum2 = LancasterBlanchard(xM, q, m)
+        
+        # First estimate (only if left branch)
+        if branch == 'left':
+            x = np.sqrt(TmTM/(Tpp/2. + TmTM/(1.-xM)**2.))
+            W = xM + x
+            W = 4.*W/(4. + TmTM) + (1. - W)**2.
+            x0 = x*(1. - (1. + m + (dtheta - 1./2.)) / (1. + 0.15*m)*x*(W/2. + 0.03*x*np.sqrt(W))) + xM
+            
+            # First estimate might not be able to yield possible solution
+            if x0 > 1.:
+                exit_flag = -1
+                return v0_vect, vf_vect, extremal_distances, exit_flag
+        
+        # Second estimate
+        else:
+            if Td > 0.:
+                x0 = xM - np.sqrt(TM/(Tpp/2. - TmTM*(Tpp/2./T0mTM - 1./xM**2.)))
+            else:
+                x00 = Td/(4. - Td)
+                W = x00 + 1.7*np.sqrt(2.*(1. - phr))
+                if W >= 0.:
+                    x03 = x00
+                else:
+                    x03 = x00 - np.sqrt((-W)**(1./8.))*(x00 + np.sqrt(-Td/(1.5*T0 - Td)))
+                W = 4./(4. - Td)
+                Lambda = (1. + (1. + m + 0.24*(dtheta - 1./2.)) / (1. + 0.15*m)*x03*(W/2. - 0.03*x03*np.sqrt(W)))
+                x0 = x03*Lambda
+                
+            # Estimate might not give solution
+            if x0 < -1.:
+                exit_flag = -1
+                return v0_vect, vf_vect, extremal_distances, exit_flag
+        
+    
+    
+    
+    # Find root of Lancaster and Blanchard's function
+    # (Halley's method)
+    x = x0
+    Tx = np.inf
+    iters = 0
+    
+    
+    
+    
+    % find root of Lancaster & Blancard's function
+    % --------------------------------------------
+
+    % (Halley's method)
+    x = x0; Tx = inf; iterations = 0;
+    while abs(Tx) > tol
+        % iterations
+        iterations = iterations + 1;
+        % compute function value, and first two derivatives
+        [Tx, Tp, Tpp] = LancasterBlanchard(x, q, m);
+        % find the root of the *difference* between the
+        % function value [T_x] and the required time [T]
+        Tx = Tx - T;
+        % new value of x
+        xp = x;
+        x  = x - 2*Tx*Tp ./ (2*Tp^2 - Tx*Tpp);
+        % escape clause
+        if mod(iterations, 7), x = (xp+x)/2; end
+        % Halley's method might fail
+        if iterations > 25, exitflag = -2; return; end
+    end
+
+    % calculate terminal velocities
+    % -----------------------------
+
+    % constants required for this calculation
+    gamma = sqrt(muC*s/2);
+    if (c == 0)
+        sigma = 1;
+        rho   = 0;
+        z     = abs(x);
+    else
+        sigma = 2*sqrt(r1*r2/(c^2)) * sin(dth/2);
+        rho   = (r1 - r2)/c;
+        z     = sqrt(1 + q^2*(x^2 - 1));
+    end
+
+    % radial component
+    Vr1    = +gamma*((q*z - x) - rho*(q*z + x)) / r1;
+    Vr1vec = Vr1*r1unit;
+    Vr2    = -gamma*((q*z - x) + rho*(q*z + x)) / r2;
+    Vr2vec = Vr2*r2unit;
+
+    % tangential component
+    Vtan1    = sigma * gamma * (z + q*x) / r1;
+    Vtan1vec = Vtan1 * th1unit;
+    Vtan2    = sigma * gamma * (z + q*x) / r2;
+    Vtan2vec = Vtan2 * th2unit;
+
+    % Cartesian velocity
+    V1 = Vtan1vec + Vr1vec;
+    V2 = Vtan2vec + Vr2vec;
+
+    % exitflag
+    exitflag = 1; % (success)
+
+    % also determine minimum/maximum distance
+    a = s/2/(1 - x^2); % semi-major axis
+    extremal_distances = minmax_distances(r1vec, r1, r1vec, r2, dth, a, V1, V2, m, muC);
+
+    
+    
+    
+    return v0_vect, vf_vect, extremal_distances, exit_flag
+
+
+def LancasterBlanchard(x, q, m):
+    
+    # Verify input
+    if x < -1.:
+        x = abs(x) - 2.
+    elif x == -1.:
+        x += np.finfo(float).eps
+        
+    # Compute parameter E
+    E = x*x - 1.
+    
+    # Compute T(x) and derivatives
+    if x == 1:
+        
+        # Parabolic, solutions known exactly
+        T = (4./3.)*(1. - q**3.)
+        Tp = (4./5.)*(q**5. - 1.)
+        Tpp = Tp + (120./70.)*(1. - q**7.)
+        Tppp = 3.*(Tpp - Tp) + (2400./1080.)*(q**9. - 1.)
+        
+    elif abs(x-1) < 1e-2:
+        
+        # Near-parabolic, compute with series
+        sig1, dsigdx1, d2sigdx21, d3sigdx31 = compute_sigmax(-E)
+        sig2, dsigdx2, d2sigdx22, d3sigdx32 = compute_sigmax(-E*q*q)
+        
+        T = sig1 - q**3.*sig2
+        Tp = 2.*x*(q**5.*dsigdx2 - dsigdx1)
+        Tpp = Tp/x + 4.*x**2.*(d2sigdx21 - q**7.*d2sigdx22)
+        Tppp = 3.*(Tpp-Tp/x)/x + 8.*x*x*(q**9.*d3sigdx32 - d3sigdx31)
+        
+    else:
+        
+        # All other cases
+        y = np.sqrt(abs(E))
+        z = np.sqrt(1. + q**2.*E)
+        f = y*(z - q*x)
+        g = x*z - q*E
+        
+        if E < 0.:
+            d = math.atan2(f, g) + math.pi*m
+        elif E == 0.:
+            d = 0.
+        else:
+            d = math.log(max(0., (f+g)))
+            
+
+        T = 2.*(x - q*z - d/y)/E
+        Tp = (4. - 4.*q**3.*x/z - 3.*x*T)/E
+        Tpp = (-4.*q**3./z * (1. - q**2.*x**2./z**2.) - 3.*T - 3.*x*Tp)/E
+        Tppp = (4.*q**3./z**2.*((1. - q**2.*x**2./z**2.) + 2.*q**2.*x/z**2.*(z - x)) - 8.*Tp - 7.*x*Tpp)/E
+
+    return T, Tp, Tpp, Tppp
+    
+
+def compute_sigmax(y):
+    '''
+    
+    '''
+    
+    # Twenty-five factors more than enough for 16-digit precision
+    an = [4.000000000000000e-001, 2.142857142857143e-001, 4.629629629629630e-002,
+          6.628787878787879e-003, 7.211538461538461e-004, 6.365740740740740e-005,
+          4.741479925303455e-006, 3.059406328320802e-007, 1.742836409255060e-008,
+          8.892477331109578e-010, 4.110111531986532e-011, 1.736709384841458e-012,
+          6.759767240041426e-014, 2.439123386614026e-015, 8.203411614538007e-017,
+          2.583771576869575e-018, 7.652331327976716e-020, 2.138860629743989e-021,
+          5.659959451165552e-023, 1.422104833817366e-024, 3.401398483272306e-026,
+          7.762544304774155e-028, 1.693916882090479e-029, 3.541295006766860e-031,
+          7.105336187804402e-033]
+    
+
+    # powers of y
+    powers = [y**exponent for exponent in range(1, 26)]
+    
+    # Vectorize
+    powers = np.reshape(powers, (25, 1))
+    an = np.reshape(an, (25, 1))
+    deriv_factors = np.reshape(range(1,26), (25,1))
+    deriv2_factors = np.reshape(range(0,25), (25,1))
+    deriv3_factors = np.reshape(range(-1, 24), (25,1))
+
+    # sigma itself
+    sig = float(4./3. + np.dot(powers.T, an))
+
+    # dsigma / dx (derivative)
+    first_der = np.reshape(np.insert(powers, 0, 1), (26, 1))
+    dsigdx = float(np.dot(np.multiply(deriv_factors, first_der[0:25]).T, an))
+
+    # d2sigma / dx2 (second derivative)
+    second_der = np.reshape(np.insert(powers, 0, np.array([1./y, 1.])), (27, 1))
+    d2sigdx2 = float(np.dot(np.multiply(np.multiply(deriv_factors, deriv2_factors), second_der[0:25]).T, an))
+    
+    # d3sigma / dx3 (third derivative)
+    third_der = np.reshape(np.insert(powers, 0, np.array([1./y/y, 1./y, 1.])), (28, 1))
+    d3sigdx3 = float(np.dot(np.multiply(np.multiply(np.multiply(deriv_factors, deriv2_factors), deriv3_factors), third_der[0:25]).T, an))
+
+    
+    return  sig, dsigdx, d2sigdx2, d3sigdx3
     
 
 def compute_extremal_dist(r0_vect, rf_vect, v0_vect, vf_vect, dtheta, a, m, GM,
--- iod/unit_test_iod.py
+++ iod/unit_test_iod.py
@@ -1,5 +1,5 @@
 import numpy as np
-from math import pi, asin, atan2
+import math
 import matplotlib.pyplot as plt
 import sys
 from datetime import datetime, timedelta
@@ -97,6 +97,96 @@ def lambert_test():
     print(extremal_distances)
     print(exit_flag)
     
+    v0_err = v0_vect - truth_dict[t0][3:6]
+    vf_err = vf_vect - truth_dict[tf][3:6]
+    
+    print(v0_err)
+    print(vf_err)
+    
+    
+    return
+
+
+def test_sigmax():
+    
+    y = -1.5
+    sig1, dsigdx1, d2sigdx21, d3sigdx31 = iod.compute_sigmax(y)
+    
+    print(sig1, dsigdx1, d2sigdx21, d3sigdx31)
+    
+    return
+
+
+def test_LancasterBlanchard():
+    
+    # Define state parameters
+    state_params = {}
+    state_params['GM'] = GME
+
+    
+    # Integration function and additional settings
+    int_params = {}
+    int_params['integrator'] = 'ode'
+    int_params['ode_integrator'] = 'dop853'
+    int_params['intfcn'] = dyn.ode_twobody
+    int_params['rtol'] = 1e-12
+    int_params['atol'] = 1e-12
+    int_params['time_format'] = 'datetime'
+    
+    # Initial object state vector
+    # Sun-Synch Orbit
+    Xo = np.reshape([757.700301, 5222.606566, 4851.49977,
+                     2.213250611, 4.678372741, -5.371314404], (6,1))
+    
+    # Propagate several orbit fractions
+    tvec = np.asarray([0., 20.*60., 70.*60.])
+    UTC0 = datetime(1999, 10, 4, 1, 45, 0)
+    tk_list = [UTC0 + timedelta(seconds=ti) for ti in tvec]
+    
+    
+    # Generate truth and measurements
+    truth_dict = {}
+    X = Xo.copy()
+    for kk in range(len(tk_list)):
+        
+        if kk > 0:
+            tin = [tk_list[kk-1], tk_list[kk]]
+            tout, Xout = dyn.general_dynamics(X, tin, state_params, int_params)
+            X = Xout[-1,:].reshape(6, 1)
+        
+        truth_dict[tk_list[kk]] = X
+        
+        
+#    print(truth_dict)
+    
+    # Setup and run Lambert Solvers
+    GM = state_params['GM']
+    t0 = tk_list[0]
+    tf = tk_list[1]
+    tof = (tf - t0).total_seconds()
+    r0_vect = truth_dict[t0][0:3]
+    rf_vect = truth_dict[tf][0:3]
+    
+    r0 = np.linalg.norm(r0_vect)
+    rf = np.linalg.norm(rf_vect)
+    r0_hat = r0_vect/r0                        
+    rf_hat = rf_vect/rf 
+    dtheta = math.acos(max(-1, min(1, np.dot(r0_hat.T, rf_hat))))
+    
+    c = np.sqrt(r0**2. + rf**2. - 2.*r0*rf*math.cos(dtheta))
+    s = (r0 + rf + c)/2.
+    T = np.sqrt(8.*GM/s**3.) * tof
+    q = np.sqrt(r0*rf)/s * math.cos(dtheta/2.)
+    
+    
+    print('q', q)
+    
+    x = 0.5
+    m = 0
+    
+    
+    T, Tp, Tpp, Tppp = iod.LancasterBlanchard(x, q, m)
+    print(T, Tp, Tpp, Tppp)
     
     return
 
@@ -104,9 +194,11 @@ def lambert_test():
 if __name__ == '__main__':
     
     
-    lambert_test()
+#    lambert_test()
     
+#    test_sigmax()
     
+#    test_LancasterBlanchard()
     
     
     
